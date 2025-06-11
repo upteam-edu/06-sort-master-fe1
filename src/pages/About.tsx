c4c6dc240990e0963e801d32928c40ce19a6f3e7
Olha8872 <situk1981@gmail.com>
2025-06-11 18:06:06 +0200

    Add description to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,10 @@
 export default function About() {
-
-  return <div>About Page</div>;
-
   return (
     <div>
       <h1>About This Project</h1>
-      <p>This project is a fork of <strong>06-SortMeister</strong>.</p>
-      <p>Created and updated by <strong>Olha8872</strong>.</p>
-      <p>You can write any description here that explains the purpose and functionality of the project.</p>
+      <p>This project is a fork of 06-SortMeister.</p>
+      <p>Created and updated by <strong>Olha Sitiuk</strong>.</p>
+      <p>This page describes the purpose and functionality of the project.</p>
     </div>
   );
-
 }
